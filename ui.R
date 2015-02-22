3a0f256af5af40ab59d8ed613c1092f61e797560
Brian Weeden <blweeden@gmail.com>
2015-02-22 15:44:06 -0500

    Fixing issues

--- ui.R
+++ ui.R
@@ -27,10 +27,7 @@ shinyUI(
         designing a car.  Use the inputs on the left panel to design your car, and then view the results 
         above in the main panel.  Results are computed immediately upon making a change to a design 
         parameter."),
-      p("
-
-
-          This applicaton uses data from a 1974 Motor Trend survey, and so is not
+      p("This applicaton uses data from a 1974 Motor Trend survey, and so is not
         based on cars that use modern technology.  Also, there are not a lot of realistic restrictions on 
         inputs, so if you're not careful, it is possible to design an unrealistic vechicle. Please note that 
         the weight is in thousands of pounds.")
